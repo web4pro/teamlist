d8f2ae3600d57bd6333d23027529fae0ac1c39d2
Azuborev <andrey.zuborev@ukad-group.com>
2026-05-07 14:05:50 +0300

    fix img

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -74,7 +74,12 @@
                             <div class="form-group row">
                                 <div class="col-md-6 offset-md-4">
                                     <a href="{{ url('login/google') }}" class="btn btn-link">
-                                        <img width="20px" style="margin-bottom:3px; margin-right:5px" alt="Google sign-in" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png" />
+                                        <img
+                                            width="20"
+                                            style="margin-bottom:3px; margin-right:5px"
+                                            alt="Google sign-in"
+                                            src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg"
+                                        />
                                         Login With Google
                                     </a>
                                     @if(session('error'))
